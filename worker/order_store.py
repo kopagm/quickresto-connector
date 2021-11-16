d95be36e130055dc7184a8bc93e06c03935e889e
Andrey Kopeykin <kopagm@gmail.com>
2021-11-16 10:56:03 +0400

    add output count

--- worker/order_store.py
+++ worker/order_store.py
@@ -1,3 +1,5 @@
+import sys
+from datetime import datetime
 from multiprocessing import JoinableQueue
 
 from worker.worker import Worker
@@ -5,21 +7,22 @@ from worker.worker import Worker
 
 class OrderStore(Worker):
 
-    def __init__(self, servers_groups: list, db_conf, *args, **kwargs):
-        self.servers_groups = servers_groups
+    def __init__(self, db_conf: dict, *args, **kwargs):
         self.db_conf = db_conf
-        # self.db = db_conf['connection'](**db_conf['sql_server'])
-        # print(db_conf)
-
 
     def store(self, item, db):
         order_table_name = item['order_table_name']
         df = item['df']
         if len(df):
-            db.update_df(table=order_table_name, df=df)
+            count = db.update_df(table=order_table_name, df=df)
+            print(f'[{datetime.now().strftime("%m.%d.%Y %H:%M:%S")}] '
+                  f'[{df.loc[0, "ServerName"]}] [{df.loc[0, "Date"]}] '
+                  f'[store {count} rows]')
+        sys.stdout.flush()
 
     def run(self, queue_in: JoinableQueue) -> None:
         db = self.db_conf['connection'](**self.db_conf['sql_server'])
+
         while True:
             item = queue_in.get()
             self.store(item, db)
